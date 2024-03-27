5952bc89713236605ea32f5f096450b22d4ed5c3
Bella <annabellaputridirgo@gmail.com>
2024-03-27 11:52:32 +0800

    fix: DRY out Modern Symmetric Cipher testcase

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -7,48 +7,34 @@ require_relative '../sk_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
+  @key = 3
+
   before do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
                          'Soumya Ray', 'Visa')
-    @key = 3
   end
 
   # Define a list of ciphers to test
   ciphers = [
-    { module: SubstitutionCipher::Caesar, name: 'Caesar cipher' },
-    { module: SubstitutionCipher::Permutation, name: 'Permutation cipher' },
-    { module: DoubleTranspositionCipher, name: 'Double Transposition Cipher' }
+    { module: SubstitutionCipher::Caesar, name: 'Caesar cipher', key: @key},
+    { module: SubstitutionCipher::Permutation, name: 'Permutation cipher', key: @key},
+    { module: DoubleTranspositionCipher, name: 'Double Transposition Cipher', key: @key },
+    { module: ModernSymmetricCipher, name: 'Modern Symmetric Cipher', key: ModernSymmetricCipher.generate_new_key }
   ]
 
   ciphers.each do |cipher|
     describe "Using #{cipher[:name]}" do
       it 'should encrypt card information' do
-        enc = cipher[:module].encrypt(@cc, @key)
+        enc = cipher[:module].encrypt(@cc, cipher[:key])
         _(enc).wont_equal @cc.to_s
         _(enc).wont_be_nil
       end
 
       it 'should decrypt text' do
-        enc = cipher[:module].encrypt(@cc, @key)
-        dec = cipher[:module].decrypt(enc, @key)
+        enc = cipher[:module].encrypt(@cc, cipher[:key])
+        dec = cipher[:module].decrypt(enc, cipher[:key])
         _(dec).must_equal @cc.to_s
       end
     end
   end
-
-  describe 'Using ModernSymmetricCipher' do
-    newkey = ModernSymmetricCipher.generate_new_key
-    it 'should encrypt card information' do
-      enc = ModernSymmetricCipher.encrypt(@cc, newkey)
-      _(enc).wont_equal @cc.to_s
-      _(enc).wont_be_nil
-    end
-
-    it 'should decrypt text' do
-      enc = ModernSymmetricCipher.encrypt(@cc, newkey)
-      dec = ModernSymmetricCipher.decrypt(enc, newkey)
-      _(dec).must_equal @cc.to_s
-    end
-  end
-
 end
